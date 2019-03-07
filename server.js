4083f553693620a1daad4e350acbb81ba1c90f38
Alisson Galiza <alissongaliza@gmail.com>
2019-03-06 22:52:33 -0300

    New types and queries on GraphQL
    + Type Streaming and Company deals with Availability of movies on the application.
    + Now we can seach for availability through the searchAvailabilityByTitle endpoint

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require("express");
 var graphqlHTTP = require("express-graphql");
 var { buildSchema } = require("graphql");
 var axios = require("axios");
+const _ = require('lodash')
+const JustWatch = require("justwatch-api");
 
-const BASE_URL = "https://api.themoviedb.org/3";
+const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 // const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-const api_key = "dc1a229b6a5f81208412ed5e273fe045";
+const TMDB_API_KEY = "dc1a229b6a5f81208412ed5e273fe045";
+
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -38,9 +41,23 @@ var schema = buildSchema(`
     name: String!
   }
 
+  type Streaming {
+    id: Int!
+    web_url: String
+    android_url: String
+    ios_url: String
+  }
+
+  type Company {
+    id: Int!
+    name: String!
+  }
+
   type Query {
     searchByTitle(title:String!): [TinyMovie]
     searchById(id:Int!): Movie
+    searchAvailabilityByTitle(title:String!): [Streaming]
+    searchProviders: [Company]
   }
 
   fragment movieInfo on Movie {
@@ -70,19 +87,44 @@ var schema = buildSchema(`
 var root = {
   searchByTitle: ({ title }) => {
     return axios
-      .get(`${BASE_URL}/search/movie`, { params: { api_key, query: title } })
+      .get(`${TMDB_BASE_URL}/search/movie`, {
+        params: { api_key: TMDB_API_KEY, query: title }
+      })
       .then(({ data }) => data.results)
       .catch(e => e);
   },
   searchById: ({ id }) => {
     return axios
-      .get(`${BASE_URL}/movie/${id}`, { params: { api_key } })
+      .get(`${TMDB_BASE_URL}/movie/${id}`, {
+        params: { api_key: TMDB_API_KEY }
+      })
       .then(({ data }) => data)
       .catch(e => e);
   },
+  searchProviders: () => {
+    return new JustWatch().getProviders().then(e =>
+      e.map(({ id, clear_name }) => {
+        return { id, name: clear_name };
+      })
+    );
+  },
+  searchAvailabilityByTitle: async ({ title }) => {
+    const movies = await new JustWatch().search({ query: title });
+
+    let providers = movies.items[0].offers.map(({ provider_id, urls }) => {     
+      //each resolution (hd,sd,etc) is returned as a new provider, but I'm only interested in the provider id, so I still have to remove the duplicates
+      return {
+        id: provider_id,
+        web_url: urls.standard_web,
+        android_url: urls.deeplink_android,
+        ios_url: urls.deeplink_ios
+      };
+    });
+    return _.uniqBy(providers,'id');  //removes duplicates
+  }
   // searchMoviePoster:({ path }) => {
   //   return axios
-  //     .get(`${IMAGE_URL}/${path}`, { params: { api_key } })
+  //     .get(`${IMAGE_URL}/${path}`, { params: { api_key:TMDB_API_KEY } })
   //     .then(({ data }) => data)
   //     .catch(e => e);
   // }
