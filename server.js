85e14c6f8439659016ce822916fb3ff85631cc50
Alisson Galiza <alissongaliza@gmail.com>
2019-04-26 18:57:39 -0300

    Getting Companies info

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const { ApolloServer, gql } = require('apollo-server')
 var axios = require("axios");
 const _ = require('lodash');
 const JustWatch = require("justwatch-api");
-const image2base64 = require('image-to-base64')
 
 
 const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
+const COMPANY_LOGO = 'https://images.justwatch.com'
+const ICON_SIZE = 's100/'
 const TMDB_API_KEY = "dc1a229b6a5f81208412ed5e273fe045";
 
 
@@ -27,6 +28,7 @@ const typeDefs = gql`
     vote_count: Int
     poster_path: String
     poster_fullPath: String
+    streamingServices: [Streaming]
   }
 
   type Genre {
@@ -35,7 +37,7 @@ const typeDefs = gql`
   }
 
   type Streaming {
-    id: Int!
+    company: Company!
     web_url: String
     android_url: String
     ios_url: String
@@ -44,6 +46,7 @@ const typeDefs = gql`
   type Company {
     id: Int!
     name: String!
+    iconURL: String!
   }
 
   type Query {
@@ -62,7 +65,7 @@ const resolvers = {
                 .get(`${TMDB_BASE_URL}/search/movie`, {
                     params: { api_key: TMDB_API_KEY, query: title }
                 })
-                .then(( data ) => data.data.results)
+                .then((data) => data.data.results)
                 .catch(e => e);
         },
         searchById: (obj, { id }, context, info) => {
@@ -75,12 +78,12 @@ const resolvers = {
                 .then(({ data }) => console.log(data))
                 .catch(e => e);
         },
-        searchPopularMovies: (obj, args , context, info) => {
+        searchPopularMovies: (obj, args, context, info) => {
             return axios
                 .get(`${TMDB_BASE_URL}/movie/popular`, {
                     params: { api_key: TMDB_API_KEY }
                 })
-                .then(({data})=>{
+                .then(({ data }) => {
                     console.log('Searched Popular Movies')
                     return data.results
                 })
@@ -109,15 +112,51 @@ const resolvers = {
             return _.uniqBy(providers, 'id');  //removes duplicates
         }
     },
-    Movie:{
-        poster_fullPath: ({poster_path})=>{
+    Movie: {
+        poster_fullPath: ({ poster_path }) => {
             return `${IMAGE_URL}${poster_path}`
+        },
+        streamingServices:async ({title}) => {
+            const movies = await new JustWatch().search({ query: title });
+
+            let providers = movies.items[0].offers.map(({ provider_id, urls }) => {
+                //each resolution (hd,sd,etc) is returned as a new provider, but 
+                //I'm only interested in the provider id, so I still have to remove the duplicates
+                return {
+                    id: provider_id,
+                    web_url: urls.standard_web,
+                    android_url: urls.deeplink_android,
+                    ios_url: urls.deeplink_ios
+                };
+            });
+            return _.uniqBy(providers, 'id');  //removes duplicates
+        }
+
+
+    },
+    Streaming: {
+        company: ({ id }) => {
+            return new JustWatch().getProviders().then(e => {
+
+
+                const company = e.filter(el => {
+                   return el.id == id
+                })[0]
+
+                if (!company)
+                    return null
+                else{
+                    const iconURL = `${COMPANY_LOGO}${company.icon_url.replace('{profile}',ICON_SIZE)}`
+                    return { id: company.id, name: company.clear_name, iconURL }
+                }
+            }
+            );
         }
     }
 
 };
 
-const server = new ApolloServer({ typeDefs, resolvers, cors:true });
+const server = new ApolloServer({ typeDefs, resolvers, cors: true });
 server.listen().then(({ url }) => {
     console.log(`Server ready at ${url}`);
 
